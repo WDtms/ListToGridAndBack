3d8bca234ae99ce56cc45853d7d587e2666c690a
Aleksey Shepelev <aleksey.shepelev@scompler.com>
2024-08-13 20:16:32 +0400

    Initial Commit

---
+++ ListToGridAndBack/ListToGridAndBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListToGridAndBackApp.swift
+//  ListToGridAndBack
+//
+//  Created by Aleksey Shepelev on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ListToGridAndBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
